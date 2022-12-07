1496d6a68bd4e807d9dbe48431e6c126b027f626
AbigailCY <yc5588@nyu.edu>
2022-12-07 07:18:32 -0500

    sh run

---
+++ run.sh
@@ -0,0 +1,14 @@
+for FILE in 1 2 3 4 41 5 51 6
+do
+    ./AES_SEQ $FILE
+    ./AES_OMPC $FILE
+    ./AES_OMPG $FILE
+    ./AES_CUDA $FILE
+done
+
+
+for threads in 4 8 12 16 20 24 28 32 40 50 60 80 100 200 300 400 500 600
+do 
+    ./AES_OMPC 5 $threads
+    ./AES_OMPG 5 $threads
+done  
