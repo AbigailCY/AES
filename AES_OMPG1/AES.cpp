931766533dee118a5e8fe6a9674637501ee530df
AbigailCY <yc5588@nyu.edu>
2022-12-07 04:55:23 -0500

    testG7

--- AES_OMPG1/AES.cpp
+++ AES_OMPG1/AES.cpp
@@ -117,7 +117,7 @@ void aes_encryption(unsigned char *message, unsigned char *result, unsigned char
 	if (width%KEY_BLOCK != 0) iters += 1;
 	int iter;
 
-	#pragma target teams distribute parallel for  data map(to from: message[0:width], result[0:width], keys[0:NUM_ROUNDS * KEY_BLOCK], width) private(iter)
+	// #pragma target teams distribute parallel for  data map(to from: message[0:width], result[0:width], keys[0:NUM_ROUNDS * KEY_BLOCK], width) private(iter)
 	for (iter = 0; iter < iters; iter++) {
 
 		int id = iter * KEY_BLOCK;
