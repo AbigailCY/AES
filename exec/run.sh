af62945c029910b6fa80ef6bc74d031e195d3621
AbigailCY <yc5588@nyu.edu>
2022-12-07 09:31:04 -0500

    added

--- exec/run.sh
+++ exec/run.sh
@@ -1,4 +1,4 @@
-for FILE in 1 2 3 4 41 5 51 6
+for FILE in 1 2 3 4 41 5 51 6 61
 do
     ./AES_SEQ $FILE
     ./AES_OMPC $FILE
@@ -9,8 +9,8 @@ done
 
 for threads in 4 8 12 16 20 24 28 32 40 50 60 80 100 200 300 400 500 600
 do 
-    ./AES_OMPC 5 $threads
-    nvprof ./AES_OMPG 5 $threads
+    ./AES_OMPC 51 $threads
+    nvprof ./AES_OMPG 51 $threads
 done  
 
-nvprof ./AES_CUDA 5
+nvprof ./AES_CUDA 51
