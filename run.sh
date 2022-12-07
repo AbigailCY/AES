e5015594beb5030d6cda61c0a4363969ea754041
AbigailCY <yc5588@nyu.edu>
2022-12-07 09:09:33 -0500

    sh file 1

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 for FILE in 1 2 3 4 41 5 51 6
 do
-    ./exec/AES_SEQ $FILE
-    ./exec/AES_OMPC $FILE
-    ./exec/AES_OMPG $FILE
-    ./exec/AES_CUDA $FILE
+    ./AES_SEQ $FILE
+    ./AES_OMPC $FILE
+    ./AES_OMPG $FILE
+    ./AES_CUDA $FILE
 done
 
 
 for threads in 4 8 12 16 20 24 28 32 40 50 60 80 100 200 300 400 500 600
 do 
-    nvprof ./exec/AES_OMPC 5 $threads
-    nvprof ./exec/AES_OMPG 5 $threads
+    nvprof ./AES_OMPC 5 $threads
+    nvprof ./AES_OMPG 5 $threads
 done  
+
+./AES_CUDA 5
