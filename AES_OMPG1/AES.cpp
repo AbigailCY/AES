13f419ca54a08f669482f6f28129a449c481600e
AbigailCY <yc5588@nyu.edu>
2022-12-07 04:24:30 -0500

    testG2

--- AES_OMPG1/AES.cpp
+++ AES_OMPG1/AES.cpp
@@ -163,13 +163,11 @@ void aes_encryption(unsigned char *message, unsigned char *result, unsigned char
 			iv_new[i] = res;
 		}
 
-		if (id < width - KEY_BLOCK)
-		{ 
-			for (int i = 0; i != KEY_BLOCK; ++i)
-			{
-				result[id + i] = iv_new[i];
-			}
+		for (int i = 0; i <= KEY_BLOCK && id + i < width; ++i)
+		{
+			result[id + i] = iv_new[i];
 		}
+		
 	}
 }
 
